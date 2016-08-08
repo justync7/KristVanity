211b4bd7a1d208d5022836f416a87d36362a6c2c
Justyn <justync9@gmail.com>
2016-08-08 18:56:47 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ sudo add-apt-repository http://ppa.launchpad.net/daniele.domenichelli/ppa/ubuntu
 sudo apt update
 sudo apt -y install libssl-dev
 sudo apt -y install libtclap-dev
+cmake -DCMAKE_BUILD_TYPE=DEBUG .
+make -j 4
 ./run.sh
